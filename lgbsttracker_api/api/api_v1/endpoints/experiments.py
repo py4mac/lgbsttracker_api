3ea273f4504fc23224d773a366616829919482e4
Pierre-Yves BOISBUNON <boisbu@gmail.com>
2020-04-30 17:45:20 +0200

    add new api

--- lgbsttracker_api/api/api_v1/endpoints/experiments.py
+++ lgbsttracker_api/api/api_v1/endpoints/experiments.py
@@ -2,17 +2,26 @@ from typing import List
 
 from fastapi import APIRouter, Response
 
-from lgbsttracker import get_experiment_by_uuid
-from lgbsttracker.entities import Experiment
+from lgbsttracker import get_experiment_by_uuid, create_experiment
+from lgbsttracker.entities import Experiment, ExperimentCreate
 
 router = APIRouter()
 
 
 @router.get("/", response_model=List[Experiment])
-async def read_experiments(response: Response, uuid: str = "experiment1"):
+async def get_experiments(response: Response, uuid: str = "experiment1"):
     """
     Retrieve experiments.
     """
     experiments = await get_experiment_by_uuid(uuid)
     response.headers["X-Total-Count"] = str(len(experiments))
     return experiments
+
+
+@router.post("/", response_model=Experiment)
+async def post_experiment(experiment: ExperimentCreate):
+    """
+    Create a new experiment.
+    """
+    new_experiment = await create_experiment(experiment)
+    return new_experiment
